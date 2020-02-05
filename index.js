1aceb79a3658a31fb73051525f200207014654c2
jennali <jennali@seattleacademy.org>
2020-02-05 09:33:22 -0800

    add server pages

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const port = 1504
+let counter = 0
+app.get('/', (req, res) => res.send('Hello Jenna!'))
+
+app.use(express.static('public'))
+
+app.get('/counter', function (req, res) {
+	counter = counter +1;
+  res.send('counter' + counter)
+})
+
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+	console.log(d);
+  res.send('Date' + d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
